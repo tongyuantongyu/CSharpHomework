a32fd392cbbe5a2fd1066b9bff1a68eb507b663a
TYTY <tongyuantongyu@gmail.com>
2020-03-06 10:48:09 +0800

    homework3

---
+++ Homework3/Shapes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shapes
+{
+    internal static class ShapeFactory
+    {
+        private static readonly Random R = new Random();
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        public static IShape GetShape(string shapeType)
+        {
+            switch (shapeType.ToLower())
+            {
+                case "rectangle":
+                    return new Rectangle(10 * R.NextDouble(), 10 * R.NextDouble());
+                case "square":
+                    return new Square(10 * R.NextDouble());
+                case "triangle":
+                    return new Triangle(10 * R.NextDouble(), 10 * R.NextDouble());
+                default:
+                    throw new ArgumentException("Unknown shape");
+            }
+        }
+
+        public static IShape GetShape()
+        {
+            var shapes = new List<string> {"rectangle", "square", "triangle"};
+            return GetShape(shapes[R.Next(0, 3)]);
+        }
+    }
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var area = Enumerable.Range(0, 10)
+                .Select(_ => ShapeFactory.GetShape())
+                .Sum(i =>
+                {
+                    Console.WriteLine(i.ToString());
+                    return i.Area;
+                });
+            Console.WriteLine($"Total area of the 10 shapes are {area}.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework3/Shapes/Shape.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace Shapes
+{
+    public interface IShape
+    {
+        double Area { get; }
+    }
+
+    public class Rectangle : IShape
+    {
+        // No necessity for modification ability after construction, prohibit it.
+        // Refactor into property and internal valid check function for such use case.
+        // ReSharper disable MemberCanBePrivate.Global
+        // ReSharper disable MemberCanBeProtected.Global
+        public readonly double Width, Height;
+        // ReSharper restore MemberCanBeProtected.Global
+        // ReSharper restore MemberCanBePrivate.Global
+
+        public Rectangle(double w, double h)
+        {
+            if (w > 0 && h > 0)
+            {
+                Width = w;
+                Height = h;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid shape.");
+            }
+        }
+
+        public double Area => Width * Height;
+
+        public override string ToString()
+        {
+            return $"Rectangle(w={Width}, h={Height}, S={Area})";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public Square(double a) : base(a, a)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Square(a={Width}, S={Area})";
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        // ReSharper disable MemberCanBePrivate.Global
+        public readonly double Width, Height;
+        // ReSharper restore MemberCanBePrivate.Global
+
+        public Triangle(double w, double h)
+        {
+            if (w > 0 && h > 0)
+            {
+                Width = w;
+                Height = h;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid shape.");
+            }
+        }
+
+        public Triangle(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && a + c > b)
+            {
+                Width = a;
+                Height = Math.Sqrt(b * b - (a * a + b * b - c * c) / (4 * a * a));
+            }
+            else
+            {
+                throw new ArgumentException("Invalid shape.");
+            }
+        }
+
+        public double Area => Width * Height / 2;
+
+        public override string ToString()
+        {
+            return $"Triangle(w={Width}, h={Height}, S={Area})";
+        }
+    }
+}
