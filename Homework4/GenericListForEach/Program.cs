96d939e075602e9e27b97a0f73a7db8c93f91679
TYTY <tongyuantongyu@gmail.com>
2020-03-13 11:57:59 +0800

    Homework4

---
+++ Homework4/GenericListForEach/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericListForEach
+{
+    public class Node<T>
+    {
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+
+        public Node<T> Next { get; set; }
+        // ReSharper disable once MemberCanBePrivate.Global
+        public T Data { get; set; }
+    }
+
+    //泛型链表类
+    public class GenericList<T> : IEnumerable<T>
+    {
+        public delegate T FuncOnOne(T x);
+
+        public delegate T FuncOnTwo(T x, T y);
+
+        public delegate void ProcudureOnOne(T x);
+
+        private Node<T> tail;
+
+        public GenericList(T headElement)
+        {
+            tail = Head = new Node<T>(headElement);
+        }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        public Node<T> Head { get; private set; }
+
+        public void Add(T t)
+        {
+            var n = new Node<T>(t);
+            if (tail == null)
+            {
+                Head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public IEnumerable<T> ForEach(FuncOnOne func)
+        {
+            var n = Head;
+            while (n != null)
+            {
+                yield return func(n.Data);
+                n = n.Next;
+            }
+        }
+
+        public void ForEach(ProcudureOnOne func)
+        {
+            var n = Head;
+            while (n != null)
+            {
+                func(n.Data);
+                n = n.Next;
+            }
+        }
+
+        public T Reduce(FuncOnTwo func, T init)
+        {
+            var n = Head;
+            while (n != null)
+            {
+                init = func(n.Data, init);
+                n = n.Next;
+            }
+
+            return init;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ForEach(x => x).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var list = new GenericList<int>(0);
+            foreach (var i in Enumerable.Range(1, 10)) list.Add(i);
+
+            foreach (var i in list) Console.Write($"{i} ");
+            Console.WriteLine();
+            Console.WriteLine($"Total: {list.Reduce((x, y) => x + y, 0)}");
+            Console.WriteLine($"Max: {list.Reduce((x, y) => x > y ? x : y, int.MinValue)}");
+            Console.WriteLine($"Min: {list.Reduce((x, y) => x < y ? x : y, int.MaxValue)}");
+
+            var list2 = list.ForEach(x => x * x);
+            foreach (var i in list2) Console.Write($"{i} ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework4/Ticker/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Threading;
+
+namespace Ticker
+{
+    internal class Ticker
+    {
+        public delegate void TickerEventHandler(object sender, TickerEventArgs args);
+
+        private readonly Func<bool> context;
+
+        private readonly int interval;
+
+        public Ticker(int interval, Func<bool> context)
+        {
+            this.interval = interval;
+            this.context = context;
+        }
+
+        public event TickerEventHandler OnTick;
+
+        public void Start()
+        {
+            void TickerWorker()
+            {
+                Thread.Sleep(interval);
+                while (!context())
+                {
+                    OnTick?.Invoke(this, new TickerEventArgs(DateTime.Now));
+                    Thread.Sleep(interval);
+                }
+            }
+
+            ThreadStart tickerRef = TickerWorker;
+            var tickerThread = new Thread(tickerRef);
+            tickerThread.Start();
+        }
+
+        public class TickerEventArgs
+        {
+            public TickerEventArgs(DateTime t)
+            {
+                Time = t;
+            }
+
+            public DateTime Time { get; }
+        }
+    }
+
+    internal class Alarm
+    {
+        public delegate void AlarmEventHandler(object sender, Ticker.TickerEventArgs args);
+
+        private readonly DateTime time;
+        private Action cancel;
+        private bool enable;
+        private bool finished;
+        private Ticker ticker;
+
+        public Alarm(DateTime t)
+        {
+            time = t;
+            enable = false;
+        }
+
+        public bool Enable
+        {
+            get => enable;
+            set
+            {
+                if (value)
+                {
+                    if (enable) return;
+                    if (finished) throw new InvalidOperationException("Can't enable finished alarm.");
+                    var interval = time - DateTime.Now;
+                    if (interval.Ticks <= 0)
+                        throw new InvalidOperationException("Can't enable alarm with passed fire time.");
+                    var (ctx, _cancel) = Program.Context();
+                    cancel = _cancel;
+                    ticker = new Ticker(interval.Milliseconds * 10, ctx);
+                    ticker.OnTick += (_, e) =>
+                    {
+                        finished = true;
+                        cancel();
+                        OnAlarm?.Invoke(this, e);
+                    };
+                    ticker.Start();
+                    enable = true;
+                }
+                else
+                {
+                    if (!enable) throw new InvalidOperationException("Can't cancel alarm that haven't enabled.");
+                    if (finished) return;
+                    finished = true;
+                    cancel();
+                }
+            }
+        }
+
+        public event AlarmEventHandler OnAlarm;
+    }
+
+    internal static class Program
+    {
+        public static (Func<bool>, Action) Context()
+        {
+            var fired = false;
+            return (() => fired, () => fired = true);
+        }
+
+        private static void Main()
+        {
+            Console.WriteLine($"[Main  : {DateTime.Now.ToLongTimeString()}] Started.");
+            var (context, cancel) = Context();
+            var timer1 = new Ticker(1000, context);
+            var timer2 = new Ticker(1500, context);
+            timer1.OnTick += (_, e) => Console.WriteLine($"[Timer1: {e.Time.ToLongTimeString()}] Tick in 1s.");
+            timer2.OnTick += (_, e) => Console.WriteLine($"[Timer2: {e.Time.ToLongTimeString()}] Tick in 1.5s.");
+
+            var alarm = new Alarm(DateTime.Now.AddSeconds(10));
+            alarm.OnAlarm += (_, e) => Console.WriteLine($"[Alarm : {e.Time.ToLongTimeString()}] Alarm fired.");
+
+            timer1.Start();
+            timer2.Start();
+            alarm.Enable = true;
+
+            Thread.Sleep(30000);
+            cancel();
+
+            Console.WriteLine($"[Main  : {DateTime.Now.ToLongTimeString()}] Finished.");
+            Console.ReadLine();
+        }
+    }
+}
